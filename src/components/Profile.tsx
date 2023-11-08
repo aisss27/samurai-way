e2dec095601e4260e3d8adae958637b229405ea6
Aisultan Abdykerov <abdykerovaisultan@gmail.com>
2023-11-08 15:24:09 +0600

    Header, Navbar, Profile components structure

---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                    alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+            <div>
+            </div>
+        </div>
+    );
+};
+
